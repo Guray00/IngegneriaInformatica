70326d1a1b1b8221a86aee1029dd53b7a2ada5a4
fedenardii <mdigalante@gmail.com>
2024-01-28 17:51:17 +0100

    [no-log]
    [no-log]

--- SECONDO ANNO/II SEMESTRE/Ricerca operativa/Codici per Esami/Matlab [Fossari]/frac.m
+++
@@ -1,9 +0,0 @@
-function result = frac(x)
-    % parte frazionaria di un numero
-    if(x < 0)
-        f = floor(x);
-        result = x - f;
-        return;
-    end
-    result = x - floor(x);
-end
