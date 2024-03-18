9d9103bba9549ffd5041fed6d7f8b91dd7fa91db
Leonardo Giovannoni <leogiovannoni@gmail.com>
2024-03-18 21:48:40 +0100

    minor

--- PRIMO ANNO/II SEMESTRE/Algoritmi e strutture dati/Esami/Prova pratica/2023/2023-04-19/soluzione.cpp
+++ PRIMO ANNO/II SEMESTRE/Algoritmi e strutture dati/Esami/Prova pratica/2023/2023-04-19/soluzione.cpp
@@ -1,8 +1,6 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <cassert>
 #include <map>
+#include <set>
 
 struct Node {
     int label;
@@ -50,16 +48,9 @@ void insert_node_bst(Node *&n, int label) {
 // }
 
 
-struct DecreasingCompare {
-    bool operator() (int a, int b) const {
-        return a > b;
-    }
-};
-
-
 // this function is O(n * log(n)) where n is the number of nodes in the tree
 // since it is called in each node, and the complexity of the operation is O(log(n))
-int fill_map(Node *n, std::map<int, std::map<int, std::vector<Node *>, DecreasingCompare>> &nodes) {
+int fill_map(Node *n, std::map<int, std::multiset<int, std::greater<int>>> &nodes) {
     if (n == nullptr) {
         return 0;
     }
@@ -74,7 +65,7 @@ int fill_map(Node *n, std::map<int, std::map<int, std::vector<Node *>, Decreasin
     // we know that accessing a map (implemented as a RBTree in GNU C++ Library) is O(log(n)), so
     // the complexity of this operation is O(log(n_points)) + O(log(n_labels_per_point)) which can be roughly
     // approximated to O(log(nodes)) + O(log(nodes)) = O(log(nodes))
-    nodes[points][n->label].push_back(n);
+    nodes[points].insert(n->label);
     return points + tmp;
 }
 
@@ -103,43 +94,20 @@ int main() {
         insert_node_bst(node, label);
     }
    
-    // this is map from ints, which are points, to map from ints, which are labels, to vector of nodes
-    // so basically a bst built from inserting sequentially from this labels 10, 0, 30, 25, 16, 30, 25
-    // will be represented inside this structure as:
-
-    // 0: {
-    //       30: [n1, n2];
-    //       25: [n3, n4]; 
-    //       0: [n5]
-    //  };
-    // 1: { 16: [n6] };
-    // 2: { 10: [n7] };
-
-    // where n1, n2, n3, n4, n5, n6, n7 are nodes with labels 30, 30, 25, 25, 0, 16, 10 respectively
-    // and n1, n2, n3, n4, n5, n6, n7 are pointers to these nodes. 
-    // 0, 1, 2 are points, and are sorted in ascending order, and 30, 25, 0, 16, 10 are labels, and are sorted in descending order
-
-    // we could also just keep track of the number of nodes with the same label, and avoid the vector of nodes
-    // but this solution is more general and maybe more intuitive
-
-    std::map<int, std::map<int, std::vector<Node *>, DecreasingCompare>> nodes{};
+    std::map<int /* point */, std::multiset<int /* label */, std::greater<int> /* decreasing order */>> nodes{};
     
     fill_map(node, nodes);
     
     // maps are ordered, so we can just iterate through them and print the labels
     // to get the desired output. The operation is O(n) where n is the number of nodes in the tree
     // since iterate over an ordered map is O(n)
-    for (auto [_point, point_map] : nodes) {        
-        for (auto [label, n] : point_map) {
-            for (Node *scan : n) {
-                if (k == 0) {
-                    break;
-                }
-                assert(scan != nullptr);
-                assert(scan->label == label);
-                std::cout << label << std::endl;
-                k--;
+    for (auto [_point, point_multiset] : nodes) {        
+        for (int label : point_multiset) {
+            if (k == 0) {
+                break;
             }
+            std::cout << label << std::endl;
+            k--;
         }
     }
 
