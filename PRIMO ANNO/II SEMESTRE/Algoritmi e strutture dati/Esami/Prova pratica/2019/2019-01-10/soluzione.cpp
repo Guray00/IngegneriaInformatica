92086fabe7abe6797d3f75af01d1b653e264b1ce
Leonardo Giovannoni <leogiovannoni@gmail.com>
2024-03-18 11:16:53 +0100

    minor

--- PRIMO ANNO/II SEMESTRE/Algoritmi e strutture dati/Esami/Prova pratica/2019/2019-01-10/soluzione.cpp
+++ PRIMO ANNO/II SEMESTRE/Algoritmi e strutture dati/Esami/Prova pratica/2019/2019-01-10/soluzione.cpp
@@ -10,11 +10,7 @@ struct HashTable {
     std::vector<std::vector<T> > table;
     std::function<int(T)> hash;
 
-    HashTable(std::function<int(T)> hash, int size) : table{size}, hash{std::move(hash)} {
-        for (auto &v: table) {
-            v = std::vector<T>{};
-        }
-    }
+    HashTable(std::function<int(T)> hash, size_t size) : table{size}, hash{std::move(hash)} { }
 
     void insert(T value) {
         table[hash(value)].push_back(value);
@@ -29,23 +25,30 @@ struct User {
 int main() {
     int n, k, s;
     std::cin >> n >> k >> s;
+    if (s < 0) {
+        throw std::invalid_argument("s must be non-negative");
+    }
     auto hash = [=](User user) { return (A * user.id + B) % P % s; };
-    HashTable<User> table(hash, s);
+    std::vector<std::vector<User> > v{};
+    {
+        HashTable<User> table{hash, (size_t) s};
 
-    for (int i = 0; i < n; i++) {
-        int id, age;
-        std::cin >> id >> age;
-        table.insert(User{id, age});
+        for (int i = 0; i < n; i++) {
+            int id, age;
+            std::cin >> id >> age;
+            table.insert(User{id, age});
+        }
+        v = std::move(table.table);
     }
 
     // using stable sort to preserve order of index with the same number of collisions
-    std::stable_sort(table.table.begin(), table.table.end(), [](const auto &a, const auto &b) -> bool {
+    std::stable_sort(v.begin(), v.end(), [](const auto &a, const auto &b) -> bool {
         return a.size() > b.size();
     });
 
     std::optional<User> max_user = std::nullopt;
     for (int i = 0; i < k; i++) {
-        for (const auto &user: table.table[i]) {
+        for (const auto &user: v[i]) {
             if (max_user == std::nullopt
                 || user.age > max_user->age
                 || (user.age == max_user->age && user.id < max_user->id)
